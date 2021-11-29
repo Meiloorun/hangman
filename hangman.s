0705c31eb9b3e41e4b212b850f40ee0752f71c28
Meiloorun <hishaam.k@live.co.uk>
2021-11-29 20:15:46 +0000

    trying to fix readfile

--- hangman.s
+++ hangman.s
@@ -35,15 +35,25 @@
 	words:	.space 100
 
 	word1:	.asciz ""
+	word1_len:	.word 0
 	word2:	.asciz ""
+	word2_len:	.word 0
 	word3:	.asciz ""
+	word3_len:	.word 0
 	word4:	.asciz ""
+	word4_len:	.word 0
 	word5:	.asciz ""
+	word5_len:	.word 0
 	word6:	.asciz ""
+	word6_len:	.word 0
 	word7:	.asciz ""
+	word7_len:	.word 0
 	word8:	.asciz ""
+	word8_len:	.word 0
 	word9:	.asciz ""
+	word9_len:	.word 0
 	word10:	.asciz ""
+	word10_len:	.word 0
 
 
 .global main
@@ -98,7 +108,7 @@ file_err:
 	mov r7, #4
 	svc #0
 
-ReadWords:mov
+ReadWords:
 	@open the file
 	ldr r0, =word_filename		//file descriptor
 	mov r1, #0x42
@@ -119,7 +129,158 @@ ReadWords:mov
 	svc #0
 
 	mov r5, r0					//save num of characters
+	mov r4, #0
+	ldr r6, =word1_len
+	ldr r2, =word1
+read_word1:
+	ldrb r3, [r1]
+	cmp r3, #10					//check if character is a line break
+	beq read_word2
+	strb r3, [r2]
+	add r3, r3, #1
+	add r2, r2, #1
+	add r4, r4, #1
+	add r1, r1, #1
+	b read_word1
 
+read_word2:
+	str r4, [r6]
+	ldr r6, =word2_len
+	mov r4, #0
+	ldr r2, =word2
+	ldrb r3, [r1]
+	mov r4, #0
+	cmp r3, #10					//check if character is a line break
+	beq read_word3
+	strb r3, [r2]
+	add r3, r3, #1
+	add r2, r2, #1
+	add r4, r4, #1
+	b read_word2
+
+read_word3:
+	str r4, [r6]
+	ldr r6, =word3_len
+	mov r4, #0
+	ldr r2, =word3
+	ldrb r3, [r1]
+	cmp r3, #10					//check if character is a line break
+	beq read_word4
+	strb r3, [r2]
+	add r3, r3, #1
+	add r2, r2, #1
+	add r4, r4, #1
+	b read_word3
+
+read_word4:
+	str r4, [r6]
+	ldr r6, =word4_len
+	mov r4, #0
+	ldr r2, =word4
+	ldrb r3, [r1]
+	cmp r3, #10					//check if character is a line break
+	beq read_word5
+	strb r3, [r2]
+	add r3, r3, #1
+	add r2, r2, #1
+	add r4, r4, #1
+	b read_word4
+
+read_word5:
+	str r4, [r6]
+	ldr r6, =word5_len
+	mov r4, #0
+	ldr r2, =word5
+	ldrb r3, [r1]
+	cmp r3, #10					//check if character is a line break
+	beq read_word6
+	strb r3, [r2]
+	add r3, r3, #1
+	add r2, r2, #1
+	add r4, r4, #1
+	b read_word5
+
+read_word6:
+	str r4, [r6]
+	ldr r6, =word6_len
+	mov r4, #0
+	ldr r2, =word6
+	ldrb r3, [r1]
+	cmp r3, #10					//check if character is a line break
+	beq read_word7
+	strb r3, [r2]
+	add r3, r3, #1
+	add r2, r2, #1
+	add r4, r4, #1
+	b read_word6
+
+read_word7:
+	str r4, [r6]
+	ldr r6, =word7_len
+	mov r4, #0
+	ldr r2, =word7
+	ldrb r3, [r1]
+	cmp r3, #10					//check if character is a line break
+	beq read_word8
+	strb r3, [r2]
+	add r3, r3, #1
+	add r2, r2, #1
+	add r4, r4, #1
+	b read_word7
+
+read_word8:
+	str r4, [r6]
+	ldr r6, =word8_len
+	mov r4, #0
+	ldr r2, =word8
+	ldrb r3, [r1]
+	cmp r3, #10					//check if character is a line break
+	beq read_word9
+	strb r3, [r2]
+	add r3, r3, #1
+	add r2, r2, #1
+	add r4, r4, #1
+	b read_word8
+
+read_word9:
+	str r4, [r6]
+	ldr r6, =word9_len
+	mov r4, #0
+	ldr r2, =word9
+	ldrb r3, [r1]
+	cmp r3, #10					//check if character is a line break
+	beq read_word10
+	strb r3, [r2]
+	add r3, r3, #1
+	add r2, r2, #1
+	add r4, r4, #1
+	b read_word9
+
+read_word10:
+	str r4, [r6]
+	ldr r6, =word10_len
+	mov r4, #0
+	ldr r2, =word10
+	ldrb r3, [r1]
+	cmp r3, #10					//check if character is a line break
+	beq game
+	strb r3, [r2]
+	add r3, r3, #1
+	add r2, r2, #1
+	add r4, r4, #1
+	b read_word10
+
+game:
+	str r4, [r6]
+
+	mov r0, #1
+	ldr r1, =word1
+	ldr r2, =word1_len
+	mov r7, #4
+	svc #0
+
+
+b end
 	
 
 end:
